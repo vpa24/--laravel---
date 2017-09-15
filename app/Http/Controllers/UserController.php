e32a7343db8964a7a912d9b87e8cd8a172d6b8bc
vpa24 <vuphuonganh020497@gmail.com>
2017-09-15 13:53:41 +0700

    Thêm model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,25 +6,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
+use App\m_san_pham;
 
 class UserController extends Controller
 {
     public function index()
     {
         $thang=date('m');
-        $users = DB::table('san_pham')
-                        ->join('chi_tiet_don_hang','san_pham.ma_sp','=','chi_tiet_don_hang.ma_sp')
+    
+        $ds_san_pham=m_san_pham::join('chi_tiet_don_hang','san_pham.ma_sp','=','chi_tiet_don_hang.ma_sp')
                         ->join('don_hang','chi_tiet_don_hang.ma_don_hang','=','don_hang.ma_don_hang')
                         ->whereMonth('ngay_dat',2)
                         ->limit(12)
                         ->get();
 
-        return view('pages.index', ['users' => $users]);
-
-    //     SELECT * from sieu_thi.san_pham inner join sieu_thi.chi_tiet_don_hang on san_pham.ma_sp = chi_tiet_don_hang.ma_sp
-    // inner join sieu_thi.don_hang on chi_tiet_don_hang.ma_don_hang=don_hang.ma_don_hang
-    // where month(ngay_dat)=$thang order by so_luong desc limit 9
-
+        return view('pages.index', ['users' => $ds_san_pham]);
 
     }
 }
---
+++ app/m_san_pham.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class m_san_pham extends Model
+{
+    //model san_pham
+    protected  $table="san_pham";
+}

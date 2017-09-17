574c3a4c5c81840ee33fb1816b5fe7937d8727be
vpa24 <vuphuonganh020497@gmail.com>
2017-09-17 20:59:34 +0700

    Chỉnh sửa layouts

--- resources/views/layouts/banner_left.blade.php
+++ resources/views/layouts/banner_left.blade.php
@@ -1,5 +1,5 @@
-@extends('main')
-@section('banner')
+
+
 		<div class="banner">
 			<div class="w3l_banner_nav_left">
 				<nav class="navbar nav_bottom">
@@ -11,22 +11,22 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-				</div> 
+				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
 						<ul class="nav navbar-nav nav_1">
-							<li><a href="products.html">Branded Foods</a></li>
+							<li><a href="products.html">Đồ tươi sống</a></li>
 							<li><a href="household.html">Households</a></li>
 							<li class="dropdown mega-dropdown active">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Veggies & Fruits<span class="caret"></span></a>				
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Đồ tươi sống<span class="caret"></span></a>
 								<div class="dropdown-menu mega-dropdown-menu w3ls_vegetables_menu">
 									<div class="w3ls_vegetables">
-										<ul>	
+										<ul>
 											<li><a href="vegetables.html">Vegetables</a></li>
 											<li><a href="vegetables.html">Fruits</a></li>
 										</ul>
-									</div>                  
-								</div>				
+									</div>
+								</div>
 							</li>
 							<li><a href="kitchen.html">Kitchen</a></li>
 							<li><a href="short-codes.html">Short Codes</a></li>
@@ -38,8 +38,8 @@
 											<li><a href="drinks.html">Soft Drinks</a></li>
 											<li><a href="drinks.html">Juices</a></li>
 										</ul>
-									</div>                  
-								</div>	
+									</div>
+								</div>
 							</li>
 							<li><a href="pet.html">Pet Food</a></li>
 							<li class="dropdown">
@@ -50,60 +50,13 @@
 											<li><a href="frozen.html">Frozen Snacks</a></li>
 											<li><a href="frozen.html">Frozen Nonveg</a></li>
 										</ul>
-									</div>                  
-								</div>	
+									</div>
+								</div>
 							</li>
 							<li><a href="bread.html">Bread & Bakery</a></li>
 						</ul>
 					</div><!-- /.navbar-collapse -->
 				</nav>
 			</div>
-			<div class="w3l_banner_nav_right">
-				<section class="slider">
-					<div class="flexslider">
-						<ul class="slides">
-							<li>
-								<div class="w3l_banner_nav_right_banner">
-									<h3>Make your <span>food</span> with Spicy.</h3>
-									<div class="more">
-										<a href="products.html" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="w3l_banner_nav_right_banner1">
-									<h3>Make your <span>food</span> with Spicy.</h3>
-									<div class="more">
-										<a href="products.html" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
-									</div>
-								</div>
-							</li>
-							<li>
-								<div class="w3l_banner_nav_right_banner2">
-									<h3>upto <i>50%</i> off.</h3>
-									<div class="more">
-										<a href="products.html" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</section>
-				<!-- flexSlider -->
-					<link rel="stylesheet" href="{{asset('css/flexslider.css')}}" type="text/css" media="screen" property="" />
-					<script defer src="{{asset('js/jquery.flexslider.js')}}"></script>
-					<script type="text/javascript">
-					$(window).load(function(){
-					$('.flexslider').flexslider({
-						animation: "slide",
-						start: function(slider){
-						$('body').removeClass('loading');
-						}
-					});
-					});
-				</script>
-				<!-- //flexSlider -->
-			</div>
-			<div class="clearfix"></div>
-		</div>
-@endsection
+
+			
